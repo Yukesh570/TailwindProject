fb129e83a1bd70e5686c1e4821399b7cb21d6d54
yukesh570 <yukesh.maha10@gmail.com>
2025-06-06 23:50:44 +0545

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Layout } from "./components/Layout";
+import { Hero } from "./components/sections/Hero";
+
+
+function App() {
+
+  return <Layout title="Edge">
+    <Hero/>
+  </Layout>;
+}
+   
+  
+
+
+export default App
---
+++ src/components/elements/Navbar.tsx
@@ -0,0 +1,40 @@
+import { Container } from "../shared/container";
+
+const navItems = [
+    { href: "#", text: "home" },
+    { href: "#services", text: "Services" },
+    { href: "#about-us", text: "About Us" },
+    { href: "#features", text: "Feature" }
+
+];
+
+export const Navbar = () => {
+  return <header className="absolute inset-x-0 top-0 z-50 py-6">
+    <Container>
+        <nav className="w-full flex justify-between gap-6 relative">
+            {/* Logo */}
+            <div className="min-w-max inline-flex relative">
+            <a href="/" className="relative flex items-center gap-3">
+                <img
+            src="/cool.png"
+            alt="Website logo"
+            className="w-[60px] h-[60px] rounded-full shadow-md"
+          />
+          <div className="inline-flex text-lg font-semibold text-heading-1">EdgeAi</div>
+            </a>
+            </div> 
+            <div className="flex flex-col lg:flex-row w-full lg:justify-between lg:items-center
+            absolute top-full left-0 lg:static lg_top-0 bg-body lg:bg-transparent border-x 
+            border-x-box-border lg:border-x-0 lg:h-auto">
+            <ul className="border-t border-box-border lg:border-t-0 px-6
+            lg:px-0 pt-6 lg:pt-0 flex flex-col lg:flex-row gap-y-4 gap-x-3
+            text-lg text-heading-2 w-fall lg:justify-center lg:items-center
+            ">{navItems.map((item,key)=>(
+              <NavItem/>
+            ))}</ul>
+
+            </div>
+        </nav>
+    </Container>
+  </header>;
+};
