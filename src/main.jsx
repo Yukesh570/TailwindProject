8addd505faa044906a20f3768f90ac0f5f3cfc17
yukesh570 <yukesh.maha10@gmail.com>
2026-02-02 17:47:26 +0545

    asd

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css' // Import your global Tailwind styles
+import App from './App.tsx' // Importing App component
 import { Provider } from "react-redux";
-import { store } from "./app/store.js";
+import { store } from "./app/store.ts"; // Importing your Redux store
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>
-);
+  </StrictMode>,
+)
